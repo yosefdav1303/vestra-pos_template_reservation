2562abc175a349ada9b53a05dce1732a75e29a68
yosefdav1303 <yosefdav1303@gmail.com>
2026-06-08 06:59:52 +0700

    Add files via upload

---
+++ src/admin-main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AdminApp from './Admin.jsx';
+import './admin.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AdminApp />
+  </React.StrictMode>
+);
